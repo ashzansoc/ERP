a964bbf6c50e0173ab975aaebe542c374a2c4465
Ashutosh Mondal <ash@zansoc.com>
2026-02-06 15:47:15 +0530

    Fix: Restore original frappe.api content and append custom code

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,111 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: MIT. See LICENSE
+from contextlib import suppress
+from enum import Enum
+
+from werkzeug.exceptions import NotFound
+from werkzeug.routing import Map, Submount
+from werkzeug.wrappers import Request, Response
+
 import frappe
-from bs4 import BeautifulSoup
 from frappe import _
+from frappe.modules.utils import get_doctype_app_map
+from frappe.monitor import add_data_to_monitor
+from frappe.utils.response import build_response
+from frappe.utils.telemetry.pulse.app_heartbeat_event import capture_app_heartbeat
+
+# --- Custom Imports Start ---
+from bs4 import BeautifulSoup
 from frappe.core.api.file import get_max_file_size
 from frappe.translate import get_all_translations
 from frappe.utils import cstr, split_emails, validate_email_address
 from frappe.utils.telemetry import POSTHOG_HOST_FIELD, POSTHOG_PROJECT_FIELD
 from semantic_version import Version
+# --- Custom Imports End ---
 
+class ApiVersion(str, Enum):
+        V1 = "v1"
+        V2 = "v2"
+
+
+def handle(request: Request):
+        """
+        Entry point for `/api` methods.
+
+        APIs are versioned using second part of path.
+        v1 -> `/api/v1/*`
+        v2 -> `/api/v2/*`
+
+        Different versions have different specification but broadly following things are supported:
+
+        - `/api/method/{methodname}` will call a whitelisted method
+        - `/api/resource/{doctype}` will query a table
+                examples:
+                - `?fields=["name", "owner"]`
+                - `?filters=[["Task", "name", "like", "%005"]]`
+                - `?limit_start=0`
+                - `?limit_page_length=20`
+        - `/api/resource/{doctype}/{name}` will point to a resource
+                `GET` will return document
+                `POST` will insert
+                `PUT` will update
+                `DELETE` will delete
+        """
+
+        try:
+                endpoint, arguments = API_URL_MAP.bind_to_environ(request.environ).match()
+        except NotFound:  # Wrap 404 - backward compatiblity
+                raise frappe.DoesNotExistError
+
+        data = endpoint(**arguments)
+        if isinstance(data, Response):
+                return data
+
+        if data is not None:
+                frappe.response["data"] = data
+        data = build_response("json")
+
+        with suppress(Exception):
+                method = arguments.get("method") or frappe.form_dict.get("method")
+                doctype = arguments.get("doctype") or frappe.form_dict.get("doctype")
+                if method or doctype:
+                        app_name = None
+                        if doctype:
+                                app_name = get_doctype_app_map().get(doctype)
+                        elif method and "." in method:
+                                app_name = method.split(".", 1)[0]
+                        if app_name:
+                                add_data_to_monitor(app=app_name)
+                                capture_app_heartbeat(app_name)
+
+        return data
+
+
+# Merge all API version routing rules
+from frappe.api.v1 import url_rules as v1_rules
+from frappe.api.v2 import url_rules as v2_rules
+
+API_URL_MAP = Map(
+        [
+                # V1 routes
+                Submount("/api", v1_rules),
+                Submount(f"/api/{ApiVersion.V1.value}", v1_rules),
+                Submount(f"/api/{ApiVersion.V2.value}", v2_rules),
+        ],
+        strict_slashes=False,  # Allows skipping trailing slashes
+        merge_slashes=False,
+)
+
+
+def get_api_version() -> ApiVersion | None:
+        if not frappe.request:
+                return
+
+        if frappe.request.path.startswith(f"/api/{ApiVersion.V2.value}"):
+                return ApiVersion.V2
+        return ApiVersion.V1
+
+# --- Custom Functions Start ---
 
 def is_frappe_version(version, above=False):
 	frappe_version = Version(frappe.__version__)
@@ -106,63 +205,4 @@ def accept_invitation(key: str | None = None):
 
 	if invitation.status == "Accepted":
 		frappe.local.login_manager.login_as(invitation.email)
-		frappe.local.response["type"] = "redirect"
-		frappe.local.response["location"] = "/crm"
-
-
-@frappe.whitelist()
-def invite_by_email(emails: str, role: str):
-	frappe.only_for(["Sales Manager", "System Manager"], True)
-
-	user_roles = frappe.get_roles(frappe.session.user)
-
-	if role == "System Manager" and "System Manager" not in user_roles:
-		frappe.throw(_("You are not allowed to invite System Managers"), frappe.PermissionError)
-
-	if role not in ["System Manager", "Sales Manager", "Sales User"]:
-		frappe.throw(_("Cannot invite for this role"), frappe.PermissionError)
-
-	if not emails:
-		return
-
-	email_string = validate_email_address(emails, throw=False)
-	email_list = split_emails(email_string)
-	if not email_list:
-		return
-	existing_members = frappe.db.get_all("User", filters={"email": ["in", email_list]}, pluck="email")
-	existing_invites = frappe.db.get_all(
-		"CRM Invitation",
-		filters={
-			"email": ["in", email_list],
-			"role": ["in", ["System Manager", "Sales Manager", "Sales User"]],
-		},
-		pluck="email",
-	)
-
-	to_invite = list(set(email_list) - set(existing_members) - set(existing_invites))
-
-	for email in to_invite:
-		frappe.get_doc(doctype="CRM Invitation", email=email, role=role).insert(ignore_permissions=True)
-
-	return {
-		"existing_members": existing_members,
-		"existing_invites": existing_invites,
-		"to_invite": to_invite,
-	}
-
-
-@frappe.whitelist()
-def get_file_uploader_defaults(doctype: str):
-	max_number_of_files = None
-	make_attachments_public = False
-	if doctype:
-		meta = frappe.get_meta(doctype)
-		max_number_of_files = meta.get("max_attachments")
-		make_attachments_public = meta.get("make_attachments_public")
-
-	return {
-		"allowed_file_types": frappe.get_system_settings("allowed_file_extensions"),
-		"max_file_size": get_max_file_size(),
-		"max_number_of_files": max_number_of_files,
-		"make_attachments_public": bool(make_attachments_public),
-	}
+# --- Custom Functions End ---
